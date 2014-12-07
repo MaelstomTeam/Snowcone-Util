e3c641dd05263c29208e5b912d46b41fd38beabd
unknown <hybolic3@hotmail.com>
2014-12-08 09:26:15 +1100

    no message

--- com/maelstrom/snowcone/extendables/ExtendableItem.java
+++ com/maelstrom/snowcone/extendables/ExtendableItem.java
@@ -1,8 +1,13 @@
 package com.maelstrom.snowcone.extendables;
 
+import com.maelstrom.arcaneMechina.reference.Reference;
+
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
 
 public abstract class ExtendableItem extends Item {
 	
@@ -12,10 +17,14 @@ public abstract class ExtendableItem extends Item {
 		super();
 		setUnlocalizedName(modid+"."+name);
 		mod_id = modid;
+//		this.setTextureName(Reference.MOD_ID + ":" + name);
 	}
 	
 	@SideOnly(Side.CLIENT)
     protected String getIconString(){
         return this.getUnlocalizedName().substring(5 + mod_id.length() + 1);
     }
+
+    public abstract void registerIcons(IIconRegister iicon);
+//    public abstract IIcon getIcon(ItemStack stack, int pass);
 }
