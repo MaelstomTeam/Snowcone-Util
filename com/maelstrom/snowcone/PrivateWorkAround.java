21d57f3c30b949bc4a8d71dd0d36580afce121f0
unknown <hybolic3@hotmail.com>
2015-03-23 06:35:35 +1100

    Quick Push for minor update

---
+++ com/maelstrom/snowcone/PrivateWorkAround.java
@@ -0,0 +1,30 @@
+package com.maelstrom.snowcone;
+
+import java.util.ArrayList;
+
+import com.google.common.base.Strings;
+
+import cpw.mods.fml.client.FMLClientHandler;
+import cpw.mods.fml.client.GuiModList;
+import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.common.ModContainer;
+
+public class PrivateWorkAround {
+	private static int myModLocation = -99;
+
+	public static int ModID() {
+		if (myModLocation == -99) {
+			ArrayList<ModContainer> mods = new ArrayList<ModContainer>();
+			FMLClientHandler.instance().addSpecialModEntries(mods);
+			int i = 0;
+			for (ModContainer mod : Loader.instance().getModList())
+				if (mod.getModId().equalsIgnoreCase("snowconeUtil")) {
+					myModLocation = i;
+					break;
+				}
+				else
+					i++;
+		}
+		return myModLocation;
+	}
+}
--- com/maelstrom/snowcone/Util.java
+++ com/maelstrom/snowcone/Util.java
@@ -1,22 +1,60 @@
 package com.maelstrom.snowcone;
 
+import java.awt.Color;
+import java.util.Calendar;
+
+import net.minecraft.client.Minecraft;
 import net.minecraft.init.Blocks;
+import net.minecraft.util.MathHelper;
+import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import org.lwjgl.opengl.GL11;
+
+import cpw.mods.fml.client.GuiModList;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid="snowconeUtil", name="Snowcone Util", version="1.0", useMetadata = true)
+@Mod(modid="snowconeUtil", name="Snowcone Util", version="1.2", useMetadata = true)
 public class Util {
 	
 	@Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event){
 		//print loading util
 		extendPotionAmountBy(128);
+		new PrivateWorkAround();
+		MinecraftForge.EVENT_BUS.register(this);
 	}
 	
 	@Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event){
 		
+	}
+	@SubscribeEvent
+	public void guiDraw(GuiScreenEvent.DrawScreenEvent event){
+		if(event.gui instanceof GuiModList){
+			GuiModList gui = (GuiModList) event.gui;
+			if(gui.modIndexSelected(PrivateWorkAround.ModID())){
+		        Calendar calendar = Calendar.getInstance();
+					GL11.glPushMatrix();
+			        GL11.glTranslatef((float)(300), 50.0F, 0.0F);
+					GL11.glRotated(-10, 0, 0, 1);
+			        float f1 = 1.8F - MathHelper.abs(MathHelper.sin((float)(Minecraft.getSystemTime() % 1000L) / 1000.0F * (float)Math.PI * 2.0F) * 0.1F);
+			        f1 = f1 * 100.0F / (float)(Minecraft.getMinecraft().fontRenderer.getStringWidth("HAPPY B-DAY HYBOLIC!!") + 32);
+			        GL11.glScalef(f1, f1, f1);
+					if(calendar.get(2) + 1 == 2 && calendar.get(5) == 7)
+						event.gui.drawCenteredString(Minecraft.getMinecraft().fontRenderer, "HAPPY B-DAY Nadir!!", 0, 0, Color.yellow.hashCode());
+			        else if((calendar.get(2) + 1 == 12 && calendar.get(5) == 16))   
+						event.gui.drawCenteredString(Minecraft.getMinecraft().fontRenderer, "HAPPY B-DAY SporNight!!", 0, 0, Color.yellow.hashCode());
+			        else if((calendar.get(2) + 1 == 10 && calendar.get(5) == 31))
+						event.gui.drawCenteredString(Minecraft.getMinecraft().fontRenderer, "Spooky Scary Skeletons!", 0, 0, Color.yellow.hashCode());
+			        else
+						event.gui.drawCenteredString(Minecraft.getMinecraft().fontRenderer, "Don't worry It's Lemon!", 0, 0, Color.yellow.hashCode());
+					GL11.glPopMatrix();
+			}
+		}
 	}
 	//update over each version if obfuscation name changes
 	private void extendPotionAmountBy(int amount) {
